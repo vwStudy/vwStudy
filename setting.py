255232254464f507f8e3ac648ad64fd2de1e4751
Ito Yuuki <c0b20024f3@edu.teu.ac.jp>
2023-07-19 14:51:17 +0900

    collision関数, setting変数の追加
    collision_carTocarという車同士の衝突を判定する関数を作成し追加, setting変数にVWfield_x,VWfield_y,VWfieldという変数を追加しvw.py内で使用

--- setting.py
+++ setting.py
@@ -1,13 +1,23 @@
 ###パラメーター####
 
 ##generation世代数,popuration遺伝子数
-params = {'max_num_iteration': 60, 'population_size': 60} 
+params = {'max_num_iteration': 2, 'population_size': 10} 
+
+##VWの設置範囲 n*n
+VWfield :int = 300
+
+##VWの設置範囲の開始位置(左上座標)
+VWfield_x :int = 400
+VWfield_y :int = 300
 
 ##VWの数
 VWnum :int = 5 
 
+##VWの大きさ
+VWsize :int = VWfield/VWnum
+
 ##車のスピード
-speed :float = 3.0
+speed :int = 3
 
 ##車の横幅
 car_width :float = 1.0
