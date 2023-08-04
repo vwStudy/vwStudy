5440d74ec1258580c83e7a43ca394e16b0edeb2b
junya <g2123025c4@edu.teu.ac.jp>
2023-08-04 14:34:16 +0900

    fix

--- result.py
+++ result.py
@@ -36,7 +36,6 @@ def move(x, y, dx, dy):
 
 def set_virtual_wall(GA_list):
         """
-
         遺伝的アルゴリズムの結果に対応したVWを設置する関数,VWの4つの頂点のlistと障害物の線分のlistを返す
         """
         size = setting.VWsize
@@ -45,20 +44,29 @@ def set_virtual_wall(GA_list):
         obstacles_vertex_list = []
         obstacles_line_list = []
         total_num_obstacles = 0
+        
+        ga_list = [GA_list]
+        # for i in GA_list:
+        #     print(GA_list[i])
+        #     if GA_list[i] >=1.0:
+        #         ga_list.append(1)
+        #     elif GA_list[i]<1:
+        #         ga_list.append(0)
+        # print("galist"+str(ga_list))
+
         #indexにインデックスをdeploy_checkには値(0,1)が入る.
-        for index, oneDivisionList in enumerate(GA_list):
+        for index, oneDivisionList in enumerate(ga_list):
             for twoDivisionIndex, deploy_check in enumerate(oneDivisionList):
-                if 1 == deploy_check:
+                if deploy_check >= 1:
                     total_num_obstacles += 1
                     #VWの左上, 左下, 右上, 右下を設定
                     VW_LeftUp = [(field_x + (size) * twoDivisionIndex), (field_y + (size * index))]
                     VW_LeftDown = [VW_LeftUp[0], VW_LeftUp[1] + size]
                     VW_RightUp = [VW_LeftUp[0] + size, VW_LeftUp[1]]
                     VW_RightDown = [VW_LeftUp[0] + size, VW_LeftUp[1] + size]
-                
+                    
                     obstacles_vertex_list.extend([VW_LeftUp, VW_LeftDown, VW_RightUp, VW_RightDown])
                     obstacles_line_list.extend([[VW_LeftUp, VW_LeftDown], [VW_LeftUp, VW_RightUp], [VW_RightUp, VW_RightDown], [VW_RightUp, VW_LeftDown]])
-
         return obstacles_vertex_list, obstacles_line_list
 
 
@@ -66,13 +74,10 @@ def set_virtual_wall(GA_list):
 
 def set_vertex_list(obstacle_list, car_start_goal_list):
         """
-        
         頂点のリストを作成し返す関数
         """
         vertex_list = car_start_goal_list
-        
         vertex_list.extend(obstacle_list)
-        
         return vertex_list
 
 def visibility_graph(vertex_list, obstacle_line_list):
@@ -112,8 +117,6 @@ def visibility_graph(vertex_list, obstacle_line_list):
             
                     visibility_graph_list.append(tuple(Line))
         
-        #print(visibility_graph_list)
-
         return visibility_graph_list
 
 
@@ -137,8 +140,8 @@ def dijkstra(visibility_graph_list):
 
 def main():
     #solution_list = vw.main()
-    solution_list = [0.6886917157974817, 1.7329621140729117, 0.621635982483528, 1.653291418411026, 1.869981314816912, 0.9946807098912214, 1.875357795465527, 1.3673799684304846, 1.8570713433403065, 0.027529367888641687, 0.2720953065838445, 1.6032967146085195, 0.5670306144517703, 0.0406742338244368, 1.0190273894113842, 0.795520416516071, 0.33904478122228987, 1.2584934127856424, 1.033738383265325, 0.44162554397710063, 0.18527236901158317, 0.8203674377614019, 0.5254761930474774, 0.6335307297759929, 1.2881038421862552, 0.9113234543294748, 1.5398149070226534, 0.5995173281635564, 0.8763542532327442, 1.9545771299140902, 1.459356932823466, 1.4000558708339292]
-    vw_list = np.array(solution_list).reshape(2,setting.VWnum**2).tolist()
+    solution_list = [0.15180780696053775, 0.8073581697399996, 1.477140413972299, 1.7997473606463872, 1.0130161896814958, 0.26786757982117493, 1.0266994861594445, 0.4157493750389958, 0.010426577803933412, 0.9765359409995915, 1.8540325152413883, 1.1274910728838243, 1.319590569693116, 1.449654406400599, 0.7196989355074781, 0.40116572861447586, 1.103637222919178, 0.6316405381188614, 1.8954056909891641, 1.4433933049373924, 1.9603248808320768, 0.437992781451644, 1.346275285338597, 1.690349870159493, 1.7552874033168995, 1.9486448531247733, 0.8688180227629614, 0.7212801312284212, 1.741517473148413, 0.8485090716355492, 0.3994523653006583, 1.6528081659718425, 1.1258270971857633, 1.8550045699408813, 1.462493894194725, 1.9709985177161566, 0.9894612303823174, 1.6528036258281, 0.40434071749076805, 0.08032099437243967, 1.277660414263412, 0.6809461778289496, 0.6245100542117898, 1.9633590339998621, 1.2463694298511374, 1.5782856369275582, 1.7220368477728998, 1.7435997734660043, 1.0716963642320276, 0.18906432709643428, 1.9294901555002868, 1.6542194682350937, 1.6763364360574062, 0.5223517545529688, 0.16544317976261103, 1.3738313562569902, 0.6211227771028829, 1.0955529700729336, 0.6052226082282239, 0.17161299756871373, 1.2232498457569274, 0.2584960988345433, 1.4834255322762293, 0.7920366501625498]
+    vw_list = np.array(solution_list).reshape(4,setting.VWnum**2).tolist()
     
     
     # class CarAgent():
@@ -204,7 +207,6 @@ def main():
     # CarAgent_3 = CarAgent(car3, setting.car3_start, setting.car3_goal)
     # CarAgent_4 = CarAgent(car4, setting.car4_start, setting.car4_goal)
 
-
     car1_x0 = setting.car1_STARTtoGOAL[0][0]
     car1_y0 = setting.car1_STARTtoGOAL[0][1]
     car2_x0 = setting.car2_STARTtoGOAL[0][0]
@@ -213,14 +215,11 @@ def main():
     car3_y0 = setting.car3_STARTtoGOAL[0][1]
     car4_x0 = setting.car4_STARTtoGOAL[0][0]
     car4_y0 = setting.car4_STARTtoGOAL[0][1]
-
-    vw_point_x1 = 380
-    vw_point_y1 = 200
         
-    for i in range(2):
+    for i in range(4):
         car_vw = np.array(vw_list[i]).reshape(setting.VWnum,setting.VWnum).tolist()
-        vw_point_x1 = 380
-        vw_point_y1 = 200
+        vw_point_x = setting.VWfield_x
+        vw_point_y = setting.VWfield_y
         list1 = []
         list2 = []
         list3 = []
@@ -230,40 +229,66 @@ def main():
             for k in range(setting.VWnum):
                 if car_vw[j][k]>=1:
                     if i==0:
-                        canvas.create_rectangle(vw_point_x1, vw_point_y1, vw_point_x1+25, vw_point_y1+25,fill='red')
-                        #list1.extend()
+                        canvas.create_rectangle(vw_point_x, vw_point_y, vw_point_x+setting.VWsize, vw_point_y+setting.VWsize,fill='red')
                         
                     elif i==1:
-                        canvas.create_rectangle(vw_point_x1, vw_point_y1, vw_point_x1+25, vw_point_y1+25,fill='blue')
+                        canvas.create_rectangle(vw_point_x, vw_point_y, vw_point_x+setting.VWsize, vw_point_y+setting.VWsize,fill='blue')
                         
                     elif i==2:
-                        canvas.create_rectangle(vw_point_x1, vw_point_y1, vw_point_x1+25, vw_point_y1+25,fill='yellow')
+                        canvas.create_rectangle(vw_point_x, vw_point_y, vw_point_x+setting.VWsize, vw_point_y+setting.VWsize,fill='yellow')
                     
                     elif i==3:
-                        canvas.create_rectangle(vw_point_x1, vw_point_y1, vw_point_x1+25, vw_point_y1+25,fill='black')
+                        canvas.create_rectangle(vw_point_x, vw_point_y, vw_point_x+setting.VWsize, vw_point_y+setting.VWsize,fill='black')
                     
-                    #list1.extend([setting.car1_start, setting.car1_goal, [vw_point_x1, vw_point_y1], [vw_point_x1, vw_point_y1+25], [vw_point_x1+25, vw_point_y1], [vw_point_x1+25, vw_point_y1+25]])##始点,終点と頂点リスト
-                    #list2.extend([setting.car2_start, setting.car2_goal, [vw_point_x1,vw_point_y1], [vw_point_x1, vw_point_y1+25], [vw_point_x1+25, vw_point_y1], [vw_point_x1+25, vw_point_y1+25]])
-                    #list3.extend([setting.car3_start, setting.car3_goal, [vw_point_x1,vw_point_y1], [vw_point_x1, vw_point_y1+25], [vw_point_x1+25, vw_point_y1], [vw_point_x1+25, vw_point_y1+25]])
-                    #list4.extend([setting.car4_start, setting.car4_goal, [vw_point_x1,vw_point_y1], [vw_point_x1, vw_point_y1+25], [vw_point_x1+25, vw_point_y1], [vw_point_x1+25, vw_point_y1+25]])
+                vw_point_x += setting.VWsize
+            vw_point_x = setting.VWfield_x
+            vw_point_y += setting.VWsize
 
-                vw_point_x1 += 25
-            vw_point_x1 = 380
-            vw_point_y1 += 25
-    
+    print("testttt")
+    car1_VW_list, car1_vw_line_list = set_virtual_wall(vw_list[0])
+    car2_VW_list, car2_vw_line_list = set_virtual_wall(vw_list[1])
+    car3_VW_list, car3_vw_line_list = set_virtual_wall(vw_list[2])
+    car4_VW_list, car4_vw_line_list = set_virtual_wall(vw_list[3])
 
-    car1_VW_list, car1_vw_line_list = set_virtual_wall(vw_list)
-    #print(car1_VW_list)
-    car1_start_goal_list=[setting.car1_STARTtoGOAL[0], setting.car1_STARTtoGOAL[1]]
+    car1_start_goal_list = setting.car1_STARTtoGOAL
+    car2_start_goal_list = setting.car2_STARTtoGOAL
+    car3_start_goal_list = setting.car3_STARTtoGOAL
+    car4_start_goal_list = setting.car4_STARTtoGOAL
+
     car1_vertex_list = set_vertex_list(car1_VW_list, car1_start_goal_list)
-    #print(car1_vertex_list)
+    car2_vertex_list = set_vertex_list(car2_VW_list, car2_start_goal_list)
+    car3_vertex_list = set_vertex_list(car3_VW_list, car3_start_goal_list)
+    car4_vertex_list = set_vertex_list(car4_VW_list, car4_start_goal_list)
+
+    car1_vertex_list = set_vertex_list(car1_VW_list, car1_start_goal_list)
+    car2_vertex_list = set_vertex_list(car2_VW_list, car2_start_goal_list)
+    car3_vertex_list = set_vertex_list(car3_VW_list, car3_start_goal_list)
+    car4_vertex_list = set_vertex_list(car4_VW_list, car4_start_goal_list)
+
+
     car1_vis_graph = visibility_graph(car1_vertex_list, car1_vw_line_list)
+    car2_vis_graph = visibility_graph(car2_vertex_list, car2_vw_line_list)
+    car3_vis_graph = visibility_graph(car3_vertex_list, car3_vw_line_list)
+    car4_vis_graph = visibility_graph(car4_vertex_list, car4_vw_line_list)
+    print(car1_vis_graph)
+
     car1_shortest_path, car1_shortest_length = dijkstra(car1_vis_graph)
+    car2_shortest_path, car2_shortest_length = dijkstra(car2_vis_graph)
+    car3_shortest_path, car3_shortest_length = dijkstra(car3_vis_graph)
+    car4_shortest_path, car4_shortest_length = dijkstra(car4_vis_graph)
+    print(car1_shortest_path)
+    print("testttt2222")
+
+    #car1の最短経路描画
+    for i in range(len(car1_shortest_path)-1):
+         canvas.create_line(car1_vertex_list[car1_shortest_path[i]][0],car1_vertex_list[car1_shortest_path[i]][1], car1_vertex_list[car1_shortest_path[i+1]][0],car1_vertex_list[car1_shortest_path[i+1]][1], fill = "red", width = 5)
+    #canvas.create_line(car1_vertex_list,car1_vertex_list, car1_vertex_list, car1_vertex_list, fill = "red", width = 5)
+    #canvas.create_line(car1_vertex_list, car1_vertex_list, 860, 240, fill = "red", width = 5)
 
     x_size=25
     y_size=10
 
-    #  #計算部分
+    #計算部分
     while True:
         #CarAgent_1.move(270,0)
         canvas.coords(car1, car1_x0, car1_y0, car1_x0+x_size, car1_y0+y_size)
@@ -277,58 +302,9 @@ def main():
         time.sleep(0.02)
         tk.update() #ウインド画面を更新
         
-        if car1_x0 >= 270:
+        if car1_x0 >= 500:
             break
         
-    for i in range(2):
-        #car_vw = np.array(vw_list[i]).reshape(setting.VWnum,setting.VWnum).tolist()
-
-        vw_point_x1 = 380
-        vw_point_y1 = 200
-        list1 = []
-        list2 = []
-        list3 = []
-        list4 = []
-        t=100
-        for j in range(setting.VWnum):
-            for k in range(setting.VWnum):
-                if car_vw[j][k]>=1:
-                    if i==0:
-
-                        #計算部分
-                        while True:
-                            #CarAgent_1.move(270,0)
-                            canvas.coords(car1, car1_x0, car1_y0, car1_x0+x_size, car1_y0+y_size)
-                            car1_mv=move(car1_x0, car1_y0, vw_point_x1, vw_point_y1)
-                            car1_x0+=car1_mv[0]
-                            car1_y0+=car1_mv[1]
-                            print("testes")
-                            time.sleep(0.02)
-                            tk.update() #ウインド画面を更新
-                            t-=1
-                            if t == 0:
-                                break
-                        
-                        
-                    # elif i==1:
-                    #     canvas.coords(car2, car2_x0, car2_y0, car2_x0+y_size, car2_y0+x_size)
-                    #     canvas.coords(car3, car3_x0, car3_y0, car3_x0+x_size, car3_y0+y_size)
-                    #     canvas.coords(car4, car4_x0, car4_y0, car4_x0+y_size, car4_y0+x_size)
-                        
-                    # elif i==2:
-                        
-                    
-                    # elif i==3:
-
-                    
-                    # list1.extend([setting.car1_start, setting.car1_goal, [vw_point_x1, vw_point_y1], [vw_point_x1, vw_point_y1+25], [vw_point_x1+25, vw_point_y1], [vw_point_x1+25, vw_point_y1+25]])##始点,終点と頂点リスト
-                    # list2.extend([setting.car2_start, setting.car2_goal, [vw_point_x1,vw_point_y1], [vw_point_x1, vw_point_y1+25], [vw_point_x1+25, vw_point_y1], [vw_point_x1+25, vw_point_y1+25]])
-                    # list3.extend([setting.car3_start, setting.car3_goal, [vw_point_x1,vw_point_y1], [vw_point_x1, vw_point_y1+25], [vw_point_x1+25, vw_point_y1], [vw_point_x1+25, vw_point_y1+25]])
-                    # list4.extend([setting.car4_start, setting.car4_goal, [vw_point_x1,vw_point_y1], [vw_point_x1, vw_point_y1+25], [vw_point_x1+25, vw_point_y1], [vw_point_x1+25, vw_point_y1+25]])
-
-            vw_point_x1 += 25
-            vw_point_x1 = 380
-            vw_point_y1 += 25
 
     tk.mainloop()
 
--- setting.py
+++ setting.py
@@ -1,14 +1,19 @@
 ###パラメーター####
 
 ##generation世代数,popuration遺伝子数
-params = {'max_num_iteration': 2, 'population_size': 10} 
+params = {'max_num_iteration': 10, 'population_size': 30} 
+
+
+
+##canvasの座標は1px単位
+##1px=約0.2mmとし、車の全長を約5m全幅を約2mとし、車は25px,10pxとする(1/1000スケール)
 
 ##VWの設置範囲 n*n
-VWfield :int = 400
+VWfield :int = 120
 
 ##VWの設置範囲の開始位置(左上座標)
 VWfield_x :int = 380
-VWfield_y :int = 200
+VWfield_y :int = 190
 
 ##VWの数
 VWnum :int = 4
@@ -26,10 +31,11 @@ car_length :float = 2.0
 car_width :float = 1.0
 
 ##車のスタートの座標とゴールの座標
-car1_STARTtoGOAL = ([170,240],[860,240])
+car1_STARTtoGOAL = [[245,240],[860,240]]##左車
 
-car2_STARTtoGOAL = ([440,0],[440,460])
+car2_STARTtoGOAL = [[440,115],[440,460]]##上車
 
-car3_STARTtoGOAL = ([770,240],[0,240])
+car3_STARTtoGOAL = [[560,240],[0,240]]##右車
 
-car4_STARTtoGOAL = ([440,460],[440,0])
+#car4_STARTtoGOAL = [[440,350],[440,0]]##下車
+car4_STARTtoGOAL = [[440,460],[440,115]]
