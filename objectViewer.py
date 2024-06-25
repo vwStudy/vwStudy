bf6e4d2971554c8b56265cd9ff7078e7a5a496f2
junya365 <g2123025c4@edu.teu.ac.jp>
2024-06-25 13:32:12 +0900

    fix viewer

--- objectViewer.py
+++ objectViewer.py
@@ -5,7 +5,6 @@ from matplotlib.animation import FuncAnimation
 class AnimationVisualizer:
     def __init__(self, trajectory_file='trajectory.npy', obstacles_file='obstacles.npy', end_positions_file='end_positions.npy', line_length=1.0):
         self.trajectory = np.load(trajectory_file)
-        print(self.trajectory)
         self.obstacles = np.load(obstacles_file, allow_pickle=True)
         self.end_positions = np.load(end_positions_file)
         self.line_length = line_length
@@ -19,14 +18,13 @@ class AnimationVisualizer:
         for obstacle_circle in self.obstacle_circles:
             obstacle_circle.set_center((0, 0))
         self.lines.set_data([], [])
-
+        
         for path_line in self.path_lines:
-            path_line.set_data([], []) 
-
-        return self.car_circles + self.end_circles + self.obstacle_circles + [self.lines]
+            path_line.set_data([], [])
+        
+        return self.car_circles + self.end_circles + self.obstacle_circles + [self.lines] + self.path_lines
 
     def update(self, frame):
-        #print(frame)
         positions = self.trajectory[frame]
         next_positions = self.trajectory[frame + 1] if frame + 1 < len(self.trajectory) else positions
 
@@ -38,16 +36,12 @@ class AnimationVisualizer:
                 np.append(self.path_lines[i].get_xdata(), positions[i][0]),
                 np.append(self.path_lines[i].get_ydata(), positions[i][1])
             )
+
         # 障害物の位置を生成
         for i, obstacle_circle in enumerate(self.obstacle_circles):
             obstacle_circle.set_center(self.obstacles[i])
 
-        # # 各車の終点を確認し、到達したら新しい終点を生成
-        # for i in range(len(positions)):
-        #     if np.linalg.norm(positions[i] - self.end_positions[i]) < 1e-5:  # 終点に到達したとみなす
-        #         self.end_positions[i] = np.random.randint(0, 20, size=2)  # 新しい終点をランダムに生成
-
-        # #終点の位置を更新
+        # 終点の位置を更新
         for i, end_circle in enumerate(self.end_circles):
             end_circle.set_center(self.end_positions[i])
 
@@ -62,7 +56,7 @@ class AnimationVisualizer:
             lines_y.extend([positions[i][1], future_position[1], None])
 
         self.lines.set_data(lines_x, lines_y)
-        return self.car_circles + self.end_circles + self.obstacle_circles + [self.lines]
+        return self.car_circles + self.end_circles + self.obstacle_circles + [self.lines] + self.path_lines
 
     def animate(self):
         fig, self.ax = plt.subplots()
@@ -73,23 +67,20 @@ class AnimationVisualizer:
         # 円の大きさを指定
         car_radius = 1
         end_point_radius = 0.2
-        obstacle_radius = 3
-        
+        obstacle_radius = 2.5
 
-        #print("f", len(self.trajectory))
-        ###ここで描写用の車を生成しているから、ifでスタート地点ごとに色を分けれそう
         self.car_circles = [plt.Circle((0, 0), car_radius, color='blue', fill=True) for _ in range(len(self.trajectory[0]))]
-        
         self.end_circles = [plt.Circle((0, 0), end_point_radius, color='green', fill=True) for _ in range(len(self.trajectory[0]))]
         self.obstacle_circles = [plt.Circle((0, 0), obstacle_radius, color='red', fill=True) for _ in range(len(self.obstacles))]
 
         for circle in self.car_circles + self.end_circles + self.obstacle_circles:
             self.ax.add_patch(circle)
-
+        
         self.lines, = self.ax.plot([], [], 'k-')  # 実線を設定
         
+        # 各車の軌跡用のラインを追加
         self.path_lines = [self.ax.plot([], [], lw=2, color='blue')[0] for _ in range(len(self.trajectory[0]))]
-
+        
         ani = FuncAnimation(fig, self.update, frames=len(self.trajectory), init_func=self.init, blit=True, repeat=False)
         plt.show()
 
